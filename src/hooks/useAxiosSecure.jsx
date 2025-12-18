0a30898f713871a9aefeca01672bb69673679ce6
fahad-bhuiyan <fahad.bhuiyan.495@gmai.com>
2025-12-18 17:27:20 +0600

    lines added

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router';
 import useAuth from './useAuth';
 
 const axiosSecure = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: 'https://club-sphere-server-xi.vercel.app/'
 })
 
 const useAxiosSecure = () => {
